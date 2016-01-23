1dde9f2a274d0cb26b7026c82f3780c7d01f1ad5
Marina Corona <marina.c.corona@gmail.com>
2016-01-23 10:53:41 -0700

    first test written

--- test/deaf_grandma_test.rb
+++ test/deaf_grandma_test.rb
@@ -2,6 +2,9 @@ gem 'minitest'
 require_relative './lib/deaf_grandma'
 
 class DeafGrandmaTest < Minitest::Test
-
+  def test_instance_of_deaf_grandma_created
+    granny = DeafGrandma.new
+    granny.instance_of?(DeafGrandma)
+  end
 
 end
