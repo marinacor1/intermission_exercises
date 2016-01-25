2cd4de711bf2a1e63a108a35d01058b82a46b3f1
marinacor1 <marinaspam1234@gmail.com>
2016-01-25 12:12:54 -0700

    level 4 tests for collections

--- test/collections_test.rb
+++ test/collections_test.rb
@@ -37,4 +37,24 @@ class CollectionsTest < Minitest::Test
     assert_equal ['Frank (33)', 'Stacy (15)', 'Juan(24)', 'Dom (32)', 'Steve (24)', 'Jill(24)'],collection.name_age(data)
   end
 
+  def test_method_organizes_names_by_age_order_alphabetically
+    skip
+    collection = Collections.new
+    data = [
+      ['Frank', 33],
+      ['Stacy', 15],
+      ['Juan', 24],
+      ['Dom', 32],
+      ['Steve', 24],
+      ['Jill', 24]
+    ]
+    assert_equal ['Stacy (15)', 'Jill(24)', 'Juan(24)', 'Steve (24)', 'Dom (32)', 'Frank (33)'], collection.name_age_alpha(data)
+  end
+
+  def test_hash_for_names_age
+    skip
+    
+
+  end
+
 end
