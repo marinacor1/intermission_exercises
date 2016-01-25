d796e97fe647619ec814ace5a4e734891e333440
marinacor1 <marinaspam1234@gmail.com>
2016-01-24 22:20:34 -0700

    broke all tests

--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -1,24 +1,31 @@
-gem 'minitest'
+gem 'minitest', '~> 5.2'
 require './lib/bubble_sort'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
 
 class BubbleSortTest < Minitest::Test
   def test_bubble_instance_created
-    bubble = BubbleSort.new[1, 0, 2, 3, 4, 5]
+    bubble = BubbleSort.new
     assert bubble.instance_of?(BubbleSort)
   end
 
-  def test_initial_bubble_is_array
-    skip
-    bubble = BubbleSort.new[1, 0, 2, 3, 4, 5]
-    assert_equal Array, bubble.class
+  def test_if_current_is_less_than_previous_than_values_swapped
+    bubble = BubbleSort.new
+    assert_equal [0, 1, 2, 3, 4, 5], bubble.sort([1, 0, 2, 3, 4, 5])
   end
 
-  def test_if_current_is_less_than_previous_than_values_swapped
-    skip
-    bubble = BubbleSort.new[1, 0, 2, 3, 4, 5]
-    bubble.sort
-    assert_equal 1, bubble[1]
-    assert_equal, [0, 1, 2, 3, 4, 5], bubble.sort 
+  def test_sort_works_with_more_complicated_array
+    # skip
+    bubble = BubbleSort.new
+    assert_equal [0, 1, 2, 3, 4, 5], bubble.sort([1, 0, 2, 4, 3, 5])
   end
+
+  def test_sort_works_with_multi_complex_swaps
+    bubble = BubbleSort.new
+    assert_equal [0, 1, 3, 4, 5], bubble.sort([4, 3, 5, 0, 1])
+  end
+
+
 
 end
