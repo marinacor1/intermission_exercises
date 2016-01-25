da81c52cc02553e65f2969e29b8365f2fb6693b7
marinacor1 <marinaspam1234@gmail.com>
2016-01-24 22:23:52 -0700

    farthest i have gotten

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -7,19 +7,31 @@ class BubbleSort
 
   def sort(bubble_array)
       #  binding.pry
+      #[4, 3, 5, 0, 1]
     previous_index = 0
-    current_index = 1 # 1
-    max_index = bubble_array.length - 1
-    # binding.pry
-    while previous_index < max_index #0
-      # binding.pry
+    current_index = 1
+    swap = 0
+    max_index = bubble_array.length - 1 #4
+    loop_max = (bubble_array.length * (bubble_array.length- 1)) + 1
+    #21
+    while previous_index < loop_max
+
+      #  binding.pry
+
       if bubble_array[current_index] < bubble_array[previous_index]
+        #3 <
+        swap += 1
         previous = bubble_array[previous_index]
         current = bubble_array[current_index]
         bubble_array[previous_index] = current
         bubble_array[current_index] = previous
-        previous_index +=1
-        current_index +=1
+          if previous_index + 1 < max_index #might be wrong
+           previous_index +=1
+           current_index +=1
+          else
+            previous_index = 0
+            current_index = 1
+          end
       else
         previous_index +=1
         current_index +=1
