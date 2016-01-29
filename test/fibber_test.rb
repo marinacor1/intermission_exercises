53836e05fb887566ebac5589fc57db5a6a133261
marinacor1 <marinaspam1234@gmail.com>
2016-01-28 21:26:33 -0700

    one method for fibber

--- test/fibber_test.rb
+++ test/fibber_test.rb
@@ -11,42 +11,60 @@ class FibberTest < Minitest::Test
 
   def test_third_num_is_sum_of_first_two_nums
     f = Fibber.new
-     assert_equal 1, f.quantity_test(3)
+    output = "0 1 1 "
+     assert_equal output, f.quantity(3)
   end
 meta omg: true
   def test_fourth_num_is_sum_of_1_and_1
     f = Fibber.new
-    assert_equal 2, f.quantity_test(4)
+    output = "0 1 1 2 "
+    assert_equal output, f.quantity(4)
   end
 
   def test_fifth_num_is_sum_of_1_and_2
     f = Fibber.new
-    assert_equal 3, f.quantity_test(5)
+    output = "0 1 1 2 3 "
+    assert_equal output, f.quantity(5)
   end
 
   def test_sixth_num_is_sum_of_2_and_3
+    skip
     f = Fibber.new
-    assert_equal 5, f.quantity_test(6)
+    output =
+"0 1 1 2 3
+ 5 "
+    assert_equal output, f.quantity(6)
   end
 
   def test_seventh_num_is_sum_of_5_and_3
+    skip
     f = Fibber.new
-    assert_equal 8, f.quantity_test(7)
+    output = "0 1 1 2 3
+5 8 "
+    assert_equal output, f.quantity(7)
   end
 
   def test_eighth_num_is_sum_of_5_and_8
+    skip
     f = Fibber.new
-    assert_equal 13, f.quantity_test(8)
+    output = "0 1 1 2 3
+5 8 13 "
+    assert_equal output, f.quantity(8)
   end
 
   def test_ninth_num_is_sum_of_8_and_13
+    skip
     f = Fibber.new
-    assert_equal 21, f.quantity_test(9)
+    output =
+"0 1 1 2 3
+5 8 13 21 "
+    assert_equal output, f.quantity(9)
   end
-meta omgoh: true
   def test_output_is_five_elements
+    skip 
     f = Fibber.new
-    output = "0 1 1 2 3
+    output =
+    "0 1 1 2 3
 5 8 13 21 34
 55 89 144 233 377 "
     assert_equal output, f.quantity
