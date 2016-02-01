275d502d8b872ab3e9133298311b12ac3212a165
marinacor1 <marinaspam1234@gmail.com>
2016-01-31 19:38:41 -0700

    finished riddles

---
+++ lib/strings_integers_riddles.rb
@@ -0,0 +1,38 @@
+
+
+
+
+*.length or .count
+*Yes
+*.capitalize. Error
+
+
+*.reverse
+
+word1.length == word2.length
+
+word.upcase == word
+
+word.downcase == word
+
+word.reverse == word
+
+word1.sub("i", "a")
+
+gsub
+word1.rjust(5, "*")
+word1.ljust(5, "*")
+word.1.rjust(x, "*")
+
+string = "pour"
+string.center(14, "*")
+
+string.center(x, " ")
+
+number.odd? number.even? number%2 == 0
+
+num.to_s == num.to_s.reverse
+
+num%3 == 0
+num%9 == 0
+num%x == 0
